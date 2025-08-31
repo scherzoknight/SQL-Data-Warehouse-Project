beae629d048babadd33f498658320a068d9bbf20
scherzoknight <125696801+scherzoknight@users.noreply.github.com>
2025-08-31 11:13:15 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,80 @@
 
+
+
+
+
+
+if object_id ('silver.crm_cust_info', 'U') is not null 
+	drop table silver.crm_cust_info;
+go
+create table silver.crm_cust_info(
+	cst_id INT,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_material_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date DATE
+);
+go
+
+if object_id ('silver.crm_prd_info', 'U') is not null 
+	drop table silver.crm_prd_info;
+go
+create table silver.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost int, 
+prd_line NVARCHAR(50),
+prd_start_date DATETIME,
+prd_end_date DATETIME
+);
+go
+
+if object_id ('silver.crm_sales_details', 'U') is not null 
+	drop table silver.crm_sales_details;
+go
+create table silver.crm_sales_details(
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales Int,
+	sls_quntity int,
+	sls_price int
+);
+go
+
+if object_id ('silver.erp_loc_a101', 'U') is not null 
+	drop table silver.erp_loc_a101;
+go
+create table silver.erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
+go
+
+if object_id ('silver.erp_cust_az12', 'U') is not null 
+	drop table silver.erp_cust_az12;
+go
+create table silver.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate NVARCHAR(50),
+	gen NVARCHAR(50)
+);
+go
+
+if object_id ('silver.erp_px_cat_g1v2', 'U') is not null 
+	drop table silver.erp_px_cat_g1v2;
+go 
+create table silver.erp_px_cat_g1v2 (
+	id NVARCHAR(50), 
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+go
+
